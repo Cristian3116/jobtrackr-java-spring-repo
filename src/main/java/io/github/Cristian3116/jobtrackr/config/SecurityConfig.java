1210199ee42d5ca4e2e173589bab44d4b249f4c7
CristianM <mija.cristian3101@gmail.com>
2026-02-28 20:03:30 +0200

    fix: resolve database connection and circular dependency

--- src/main/java/io/github/Cristian3116/jobtrackr/config/SecurityConfig.java
+++ src/main/java/io/github/Cristian3116/jobtrackr/config/SecurityConfig.java
@@ -4,10 +4,7 @@ import io.github.Cristian3116.jobtrackr.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -39,9 +36,5 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-    // Optional: configure DaoAuthenticationProvider if needed
+
 }
