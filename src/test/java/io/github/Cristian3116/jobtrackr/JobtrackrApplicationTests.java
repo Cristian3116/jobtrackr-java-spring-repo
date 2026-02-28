1c1f77eb746f662ba4de22a3a2457898e8ab88ee
CristianM <mija.cristian3101@gmail.com>
2026-02-28 18:40:36 +0200

    Initial commit

---
+++ src/test/java/io/github/Cristian3116/jobtrackr/JobtrackrApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Cristian3116.jobtrackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtrackrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
